ee037d7ea0587d8a2511241da80cb84f7a23175a
MadMushroom <bblack.ben11@gmail.com>
2022-11-04 12:06:22 +0100

    Fixed .gitignore

--- build-release.bat
+++
@@ -1,3 +0,0 @@
-windres resource.rc -O coff -o resource.res
-g++ -Wall -o Doggenstein main.cpp resource.res -lglu32 -lglut32 -lopengl32
-exit
--- build.bat
+++
@@ -1,5 +0,0 @@
-g++ -Wall -o main main.cpp -lglu32 -lglut32 -lopengl32
-goto %1%
-exit
-:run
-main
